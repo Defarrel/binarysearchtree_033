bb7dc06c00b1d6b06b33b4e9b481db10bc9948b7
defarrel <Defarrel>
2023-07-01 19:49:43 +0700

    Membuat switch case untuk display program

--- binarysearchtree_033/binarysearchtree_033.cpp
+++ binarysearchtree_033/binarysearchtree_033.cpp
@@ -117,5 +117,36 @@ int main() {
 		char ch;
 		cin >> ch;
 		cout << endl;
-	}
+	
+		switch (ch)
+		{
+		case '1':
+		{
+			cout << "Enter a word: ";
+			string word;
+			cin >> word;
+			obj.insert(word);
+			break;
+		}
+		case '2':
+		{
+			obj.inorder(obj.root);
+			break;
+		}
+		case '3':
+		{
+			obj.preorder(obj.root);
+			break;
+		}
+		case '4':
+		{
+			obj.postorder(obj.root);
+			break;
+		}
+		case '5':
+			return 0;
+		default:
+		{
+		}
+		}
 }
