f515fba5d1c0cf262784f67e25c720f1225e37df
defarrel <Defarrel>
2023-07-01 19:44:20 +0700

    Membuat void postorder untuk mengurutkan node dari kiri->kanan->root

--- binarysearchtree_033/binarysearchtree_033.cpp
+++ binarysearchtree_033/binarysearchtree_033.cpp
@@ -87,4 +87,16 @@ public:
 			preorder(ptr->rightchild);
 		}
 	}
+	void postorder(node* ptr)
+	{
+		if (root == NULL)
+		{
+			cout << "tree is empty" << endl;
+			return;
+		}
+		if (ptr != NULL)
+		{
+		
+		}
+	}
 };
