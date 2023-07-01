059fe012a2dae6b60254bac9fd6f9be5fab78713
defarrel <Defarrel>
2023-07-01 19:13:50 +0700

    Membuat void insert untuk operasi menginput nilai pada node yang baru ditambahkan ke memory

--- binarysearchtree_033/binarysearchtree_033.cpp
+++ binarysearchtree_033/binarysearchtree_033.cpp
@@ -23,4 +23,15 @@ public:
 	binarytree() {
 		root = NULL; //initializing root to null
 	}
+
+	void insert(string element) {
+		node* newNode = new node(element, NULL, NULL); //Allocate memory for the new node
+		newNode->info = element; // Assign value to the data filed of the new node 
+		newNode->leftchild = NULL; // Make the left child of the new  node 
+		newNode->rightchild = NULL; // Make the right child of the new node
+
+		node* parent = NULL;
+		node* currentNode = NULL;
+		search(element, parent, currentNode); // Locate the node which will be the parent of the node to be inserted
+	}
 };
